5a2844145a177389c1d8ed682c1544efa9fd9171
Andrew Sem Tetteh <andrewsemtetteh@gmail.com>
2024-08-11 01:07:47 +0000

    deployement after API exposure

--- final_project.py
+++ final_project.py
@@ -1,38 +1,38 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import os
 import replicate
 from langchain.prompts import PromptTemplate
 from langchain.llms import Replicate
 from langchain.chains import LLMChain
-import math
+from dotenv import load_dotenv
 
-# loading our API token
-os.environ["REPLICATE_API_TOKEN"] = "r8_GD9vQuJm27to5pD2NQB94N5qr5ihmgj07gWoz"
+# loading environment variables from .env file
+load_dotenv()
+replicate_api_token = os.getenv("REPLICATE_API_TOKEN")
+os.environ["REPLICATE_API_TOKEN"] = replicate_api_token
 
-# Initializing the Llama 2 model using Replicate
+# initializing the llama 2 model using replicate
 llm = Replicate(
     model="meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
     input={"temperature": 0.75, "max_length": 500, "top_p": 1}
 )
 
-#loading data directly from the excel file
-sheet_id1 = '1aR4SCyPBP3ON2yUcLFbVRKp_dQxpjudF6ceTOKLHkIA'
-diet_data = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id1}/export?format=csv')
+# loading our dataset directly from the excel file
+sheet_id_diet = '1aR4SCyPBP3ON2yUcLFbVRKp_dQxpjudF6ceTOKLHkIA'
+diet_data = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id_diet}/export?format=csv')
 
-sheet_id2 = '16f52y1BEI1KWDnS0iUgwDJ2mvmj0-NoxTsig4zBNLcw'
-workout_data = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id2}/export?format=csv')
+sheet_id_workouts = '16f52y1BEI1KWDnS0iUgwDJ2mvmj0-NoxTsig4zBNLcw'
+workout_data = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id_workouts}/export?format=csv')
 
-diet_features = diet_data[['Allergens', 'Vegetarian', 'Dietary Tags', 'Calories']]
+# preprocessing the dietary data, separating the values separated by comma's
+diet_data['Allergens'] = diet_data['Allergens'].fillna('').apply(lambda x: [item.strip() for item in str(x).split(',')] if x else [])
+diet_data['Dietary Tags'] = diet_data['Dietary Tags'].fillna('').apply(lambda x: [item.strip() for item in str(x).split(',')] if x else [])
+diet_data['Meal Type'] = diet_data['Meal Type'].fillna('').apply(lambda x: [item.strip() for item in str(x).split(',')] if x else [])
 
-# preprocessing the dietary data
-diet_data['Allergens'] = diet_data['Allergens'].apply(lambda x: [item.strip() for item in str(x).split(',')] if pd.notna(x) else [])
-diet_data['Dietary Tags'] = diet_data['Dietary Tags'].apply(lambda x: [item.strip() for item in str(x).split(',')] if pd.notna(x) else [])
-
-# reprocessing the workout data
-def categorize_activity_level(activity):
-    activity = activity.lower().strip()
+# reprocessing and categorizing the workout data
+def categorizing_activity_level(activity):
+    activity = str(activity).lower().strip()
     if activity in ['sedentary', 'low']:
         return 'Sedentary'
     elif activity in ['light', 'lightly active']:
@@ -47,9 +47,9 @@ def categorize_activity_level(activity):
         return 'Moderately Active'
 
 # applying the categorization from the processed workout data to the activity level
-workout_data['Activity Level'] = workout_data['Activity Level'].apply(categorize_activity_level)
+workout_data['Activity Level'] = workout_data['Activity Level'].apply(categorizing_activity_level)
 
-# defining the prompt template for diet and workout recommendations
+# defining the prompt template for the diet recommendations
 diet_template = PromptTemplate(
     input_variables=['Allergens', 'Vegetarian', 'Dietary_tags'],
     template="""Ghanaian Diet & Workout Recommendation System:
@@ -62,7 +62,10 @@ diet_template = PromptTemplate(
 # defining the prompt template for weight management advice
 weight_management_template = PromptTemplate(
     input_variables=["bmi", "bmr", "daily_calories", "diet", "activity_level"],
-    template="""Based on the following health metrics and user information, provide 3 to 5 personalized weight management tips in bullet points in one line sentence. don' add anything sentence before and after the points, avoid the sure, .....just list the points. After the title just list the points, no overview. write everything in the second person point of view, instead of user use you:
+    template="""State what the health metrics calculated mean and how it affects the users health, whether good  or bad in a "bullet point". The summary should be detailed. Use "your" instead of "user". :
+    
+    And then based on the summary of the health metrics and user information state 3 to 5 weight management tips for the user in "bullet points". Use "your" instead of "user". All should be in bullet points :
+
     BMI: {bmi}
     BMR: {bmr} calories/day
     Estimated Daily Caloric Needs: {daily_calories} calories/day
@@ -71,18 +74,18 @@ weight_management_template = PromptTemplate(
     """
 )
 
-# calculating the BMI based on the user's input
+# calculating the bmi based on the user's input
 def calculating_bmi(weight, height):
     return weight / ((height/100)**2)
 
-# the function to calculate the BMR
+# the function to calculate the bmr
 def calculating_bmr(weight, height, age, gender):
     if gender.lower() == 'male':
         return 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
     else:
         return 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
 
-#calculating daily caloric needs
+# calculating daily caloric needs
 def calorie_needs(bmr, activity_level):
     activity_factors = {
         'Sedentary': 1.2,
@@ -93,33 +96,33 @@ def calorie_needs(bmr, activity_level):
     }
     return bmr * activity_factors[activity_level]
 
-# function to calculate Body Fat Percentage (BFP)
+# function to calculate body fat percentage (bfp)
 def calculate_bfp(bmi, age, gender):
     if gender.lower() == 'male':
         return 1.20 * bmi + 0.23 * age - 16.2
     else:
         return 1.20 * bmi + 0.23 * age - 5.4
 
-# function to calculate Body Surface Area (BSA)
+# function to calculate body surface area (bsa)
 def calculate_bsa(weight, height):
     return 0.007184 * (weight ** 0.425) * (height ** 0.725)
 
-# function to calculate Lean Body Mass (LBM)
+# function to calculate lean body mass (lbm)
 def calculate_lbm(weight, bfp):
     return weight - (weight * bfp / 100)
 
 # function to get meal recommendations based on the user's preferences
 def meal_recommendations(meal_type, user_preferences):
-    meal_data = diet_data[diet_data['Meal Type'] == meal_type]
+    meal_data = diet_data[diet_data['Meal Type'].apply(lambda x: meal_type in x)]
     
     if meal_data.empty:
-        return pd.DataFrame(columns=['Diet Name', 'Ingredients', 'Allergens','Category', 'Vegetarian', 'Calories'])
+        return pd.DataFrame(columns=['Diet Name', 'Ingredients', 'Allergens', 'Category', 'Vegetarian', 'Calories'])
     
-    # filtering the Meal Names based on user preferences
+    # filtering the meal names based on user preferences
     if user_preferences['vegetarian'] != 'All':
         meal_data = meal_data[meal_data['Vegetarian'] == user_preferences['vegetarian']]
     
-    # Filtering out food names with user's allergens
+    # filtering out food names with user's allergens
     if user_preferences['allergies'] and 'None' not in user_preferences['allergies']:
         meal_data = meal_data[~meal_data['Allergens'].apply(lambda x: any(allergen in x for allergen in user_preferences['allergies']))]
     
@@ -140,14 +143,11 @@ def workout_recommendations(activity_level, activity_factor):
     }
     
     intensity = intensity_map[activity_level]
-    
-    # filtering workouts based on intensity
     filtered_workouts = workout_data[workout_data['Intensity'] == intensity]
     
     if filtered_workouts.empty:
         filtered_workouts = workout_data
     
-    # adjusting workout intensity based on activity factor
     if activity_factor <= 1.375:
         filtered_workouts = filtered_workouts[filtered_workouts['Intensity'].isin(['Low', 'Medium'])]
     elif activity_factor <= 1.725:
@@ -155,35 +155,27 @@ def workout_recommendations(activity_level, activity_factor):
     else:
         filtered_workouts = filtered_workouts[filtered_workouts['Intensity'] == 'High']
     
-    # getting unique workouts
     unique_workouts = filtered_workouts.drop_duplicates(subset=['Workout Name'])
-    
-    # Determining the number of recommendations for workouts
     num_unique = len(unique_workouts)
     num_recommendations = min(max(3, num_unique), 7)
     
-    # If we have enough unique workouts, sample without replacement
     if num_unique >= num_recommendations:
         recommendations = unique_workouts.sample(n=num_recommendations, replace=False)
     else:
-        # If we don't have enough unique workouts, use all unique ones and fill the rest randomly
         recommendations = pd.concat([
             unique_workouts,
             filtered_workouts.sample(n=num_recommendations-num_unique, replace=True)
         ])
     
-    # shuffling the recommendations
     recommendations = recommendations.sample(frac=1).reset_index(drop=True)
     
     return recommendations[['Workout Name', 'Duration (minutes)', 'Type of Exercise', 'Reps', 'Sets', 'Distance (km)', 'Intensity']]
 
-# Streamlit UI for the App Title and sub header
 st.markdown('<h1 style="color: orange; text-align: center; font-size: 150px;">Didi Yiye</h1>', unsafe_allow_html=True)
 st.markdown('<h2 style="text-align: center; font-size: 20px;">Your Ghanaian Diet & Workout Recommender</h2>', unsafe_allow_html=True)
 
-# accepting User input
-
-with st.expander("Enter Your Information"):
+# accepting user input
+with st.expander("Enter Your Information & Preferences"):
     age = st.number_input('Age', min_value=1, max_value=120)
     weight = st.number_input('Weight (kg)', min_value=1.0, max_value=300.0)
     height = st.number_input('Height (cm)', min_value=1, max_value=300)
@@ -194,11 +186,11 @@ with st.expander("Enter Your Information"):
     vegetarian = vegetarian_mapping[vegetarian_option]
     dietary_tags = st.selectbox('Dietary Tags', ['All', 'High in Carbs', 'High in Protein', 'High in Vegetables', 'High in Protein, Carbs', 'High in Carbs, Protein', 'High in Protein, Fat', 'High in Carbs, Spicy', 'High in Fat, Protein', 'High in Carbs, Fat', 'High in Spice'])
     activity_level = st.selectbox('Activity Level', ['Sedentary', 'Lightly Active', 'Moderately Active', 'Very Active', 'Extremely Active'])
-    
-# creating columns for buttons
+
+# creating columns for the buttons
 diet, workout, health = st.columns(3)
 
-#processing the recommendations when buttons are pressed
+# processing the recommendations when buttons are pressed
 if diet.button('Diet Recommendations'):
     user_input = {
         "Allergens": allergies,
@@ -207,37 +199,33 @@ if diet.button('Diet Recommendations'):
         "activity_level": activity_level,
     }
     
-    # Generating recommendations
+    # generating recommendations
     chain = LLMChain(llm=llm, prompt=diet_template)
-    try:
-        response = chain.run(user_input)
+    response = chain.run(user_input)
+    
+    meal_types = ["Breakfast", "Snack", "Lunch", "Dinner"]
+    
+    st.subheader("Diet Recommendations")
+    
+    for meal_type in meal_types:
+        st.markdown(f"### {meal_type}")
         
-        # Extracting the recommendations using regex
-        meal_types = ["Breakfast", "Snack", "Lunch", "Dinner"]
+        # recommendations based on the user preferences
+        user_preferences = {
+            'vegetarian': vegetarian,
+            'allergies': allergies if 'None' not in allergies else []
+        }
+        recommendations = meal_recommendations(meal_type, user_preferences)
         
-        st.subheader("Diet Recommendations")
-        
-        for meal_type in meal_types:
-            st.markdown(f"### {meal_type}")
-            
-            # Recommendations based on the user preferences
-            user_preferences = {
-                'vegetarian': vegetarian,
-                'allergies': allergies if 'None' not in allergies else []
-            }
-            recommendations = meal_recommendations(meal_type, user_preferences)
-            
-            if not recommendations.empty:
-                st.table(recommendations)
-            else:
-                st.write(f"No {meal_type.lower()} recommendations found based on your input.")
-    except Exception as e:
-        st.error(f"An error occurred while generating diet recommendations: {str(e)}")
+        if not recommendations.empty:
+            st.table(recommendations)
+        else:
+            st.write(f"No {meal_type.lower()} recommendations found based on your input.")
 
 if workout.button('Workout Recommendations'):
     st.subheader("Workout Recommendations")
     
-    # Calculate activity factor
+    # calculate activity factor
     activity_factors = {
         'Sedentary': 1.2,
         'Lightly Active': 1.375,
@@ -247,15 +235,14 @@ if workout.button('Workout Recommendations'):
     }
     activity_factor = activity_factors[activity_level]
     
-    # Workout recommendations based on activity level and factor
+    # workout recommendations based on activity level and factor
     workout_recommendations = workout_recommendations(activity_level, activity_factor)
-    
     if not workout_recommendations.empty:
         st.table(workout_recommendations.reset_index(drop=True))
     else:
         st.write("No workout recommendations found based on your activity level. You can select another level")
 
-#button to display the calculated health metrics
+# button to display the calculated health metrics
 if health.button('Health Metrics'):
     bmi = calculating_bmi(weight, height)
     bmr = calculating_bmr(weight, height, age, gender)
@@ -274,7 +261,7 @@ if health.button('Health Metrics'):
     
     activity_factor = activity_factors[activity_level]
     
-    st.subheader("Health Metrics")
+    st.subheader("Your Health Metrics")
     st.write(f"Body Mass Index (BMI): {bmi:.2f}")
     st.write(f"Basal Metabolic Rate (BMR): {bmr:.2f} calories/day")
     st.write(f"Estimated Daily Caloric Needs: {daily_calories:.2f} calories")
@@ -283,7 +270,7 @@ if health.button('Health Metrics'):
     st.write(f"Lean Body Mass (LBM): {lbm:.2f} kg")
     st.write(f"Activity Factor: {activity_factor:.2f}")
     
-    # generating weight management tips using Llama 2
+    # generating weight management tips using the llama 2 model
     weight_management_chain = LLMChain(llm=llm, prompt=weight_management_template)
     weight_management_input = {
         "bmi": f"{bmi:.2f}",
@@ -292,15 +279,11 @@ if health.button('Health Metrics'):
         "diet": dietary_tags,
         "activity_level": activity_level
     }
-    try:
-        weight_management_tips = weight_management_chain.run(weight_management_input)
-        st.subheader("Weight Management Advice")
-        st.write(weight_management_tips)
-    except Exception as e:
-        st.error(f"An error occurred while generating weight management tips: {str(e)}")
+    weight_management_tips = weight_management_chain.run(weight_management_input)
+    st.subheader("Weight Management Advice")
+    st.write(weight_management_tips)
     
-    # displaying workout recommendations based on activity factor
-    st.subheader("Workouts Based on Health Metrics")
+    st.subheader("Recommended Workouts Based on Your Health Metrics")
     workout_recommendations = workout_recommendations(activity_level, activity_factor)
     
     if not workout_recommendations.empty:
@@ -311,7 +294,7 @@ if health.button('Health Metrics'):
 # running the Streamlit app
 if __name__ == "__main__":
     st.sidebar.markdown("## About")
-    st.sidebar.info("This app recommends Ghanaian diet and workout based on your input. It uses a language model to generate recommendations and suggests suitable Food & Workouts based on your input.")
+    st.sidebar.info("This app recommends Ghanaian diets and workouts based on your input. It uses a language model to generate recommendations and suggests suitable Food & Workouts based on user input.")
     
     st.sidebar.markdown("## Notice")
-    st.sidebar.warning("NB: This app is for informational purposes only and should not replace professional medical advice of any sort")
+    st.sidebar.warning("NB: This app is for informational purposes only and should not replace professional medical advice of any sort.")
